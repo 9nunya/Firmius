bd8c40f12bdf9b955767cd0279c69cd9517924c1
9nunya <119450941+9nunya@users.noreply.github.com>
2026-04-22 21:21:22 -0400

    got CMake configuration working across platforms

--- packages/shared/src/EnvLoader.cpp
+++ packages/shared/src/EnvLoader.cpp
@@ -1,12 +1,22 @@
 #include "EnvLoader.hpp"
 #include "utils/Logger.hpp"
 
+#include <algorithm>
+#include <cstdlib>
 #include <fstream>
 #include <sstream>
-#include <cstdlib>
-#include <algorithm>
 
 namespace firmius::shared {
+
+namespace {
+void setEnvironmentVariable(const std::string& key, const std::string& value) {
+#if defined(_WIN32)
+    _putenv_s(key.c_str(), value.c_str());
+#else
+    setenv(key.c_str(), value.c_str(), 1);
+#endif
+}
+} // namespace
 
 void EnvLoader::load(const std::string& filePath) {
     std::ifstream file(filePath);
@@ -18,36 +28,36 @@ void EnvLoader::load(const std::string& filePath) {
 
     std::string line;
     while (std::getline(file, line)) {
-        // Remove comments
         auto commentPos = line.find('#');
         if (commentPos != std::string::npos) {
             line = line.substr(0, commentPos);
         }
 
-        // Trim whitespace
         line.erase(0, line.find_first_not_of(" \t\r\n"));
         line.erase(line.find_last_not_of(" \t\r\n") + 1);
 
-        if (line.empty()) continue;
+        if (line.empty()) {
+            continue;
+        }
 
         auto equalPos = line.find('=');
-        if (equalPos == std::string::npos) continue;
+        if (equalPos == std::string::npos) {
+            continue;
+        }
 
         std::string key = line.substr(0, equalPos);
         std::string value = line.substr(equalPos + 1);
 
-        // Trim key and value
         key.erase(key.find_last_not_of(" \t") + 1);
         value.erase(0, value.find_first_not_of(" \t"));
 
-        // Remove quotes if present
         if (value.size() >= 2 && value.front() == '"' && value.back() == '"') {
             value = value.substr(1, value.size() - 2);
         } else if (value.size() >= 2 && value.front() == '\'' && value.back() == '\'') {
             value = value.substr(1, value.size() - 2);
         }
 
-        setenv(key.c_str(), value.c_str(), 1);
+        setEnvironmentVariable(key, value);
     }
 }
 
@@ -56,4 +66,4 @@ std::string EnvLoader::get(const std::string& key, const std::string& defaultVal
     return val ? std::string(val) : defaultValue;
 }
 
-}
+} // namespace firmius::shared
